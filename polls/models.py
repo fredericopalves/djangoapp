e831b4f3f22357515810db7c0a7f6f6b30e5494b
Fred Alves <frederico.alves@ericsson.com>
2021-03-26 14:59:57 -0500

    chaanged model

--- polls/models.py
+++ polls/models.py
@@ -19,6 +19,7 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+    new_field=models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
